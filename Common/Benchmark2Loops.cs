c67ee1ba4b961c3a56410594d6a49330d45b6d5c
Fread75 <frederic.delanchy@gmail.com>
2019-12-14 22:59:55 +0100

    new methods

---
+++ Common/Benchmark2Loops.cs
@@ -0,0 +1,79 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Linq;
+
+namespace Common
+{
+    [DisassemblyDiagnoser(recursiveDepth: 3)]
+    //[DryJob]
+    public unsafe class Benchmark2Loops
+    {
+        private const int SIZE = 2000 * 2000;
+        private readonly double maxArrayValue = Math.Sqrt(double.MaxValue / SIZE);
+        private readonly double[] array1 = new double[SIZE];
+        private readonly double[] array2 = new double[SIZE];
+        
+        public Benchmark2Loops()
+        {
+            var random = new Random(0);
+            for (int i = 0; i < array1.Length; i++)
+            {
+                array1[i] = random.NextDouble() * maxArrayValue;
+                array2[i] = random.NextDouble() * maxArrayValue;
+            }
+        }
+ 
+        [Benchmark(Baseline = true)]
+        public void SimpleFor() => SimpleFor(array1, array2);
+        [Benchmark]
+        public void SimpleForWithCheck() => SimpleForWithCheck(array1, array2);
+        [Benchmark]
+        public void UnsafeFor() => UnsafeFor(array1, array2);
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public double SimpleFor(double[] array1, double[] array2)
+        {
+            var sum = 0D;
+            for (int i = 0; i < array1.Length; i++)
+            {
+                sum += array1[i] + array2[i];
+            }
+            return sum;
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public double SimpleForWithCheck(double[] array1, double[] array2)
+        {
+            if (array1.Length != array2.Length)
+            {
+                return 0;
+            }
+
+            var sum = 0D;
+            for (int i = 0; i < array1.Length; i++)
+            {
+                sum += array1[i] + array2[i];
+            }
+            return sum;
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public double UnsafeFor(double[] array1, double[] array2)
+        {
+            var sum = 0D;
+            fixed (double* ptr2 = array2)
+            {
+                for (int i = 0; i < array1.Length; i++)
+                {
+                    sum += array1[i] + ptr2[i];
+                }
+            }
+            return sum;
+        }
+
+    }
+}
--- Common/BenchmarkMatrixMultiplication.cs
+++ Common/BenchmarkMatrixMultiplication.cs
@@ -5,63 +5,102 @@ using Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics;
+
+using DenseMatrix = MathNet.Numerics.LinearAlgebra.Double.DenseMatrix;
 
 namespace Common
 {
-    [DisassemblyDiagnoser(printIL:true, recursiveDepth: 3)]
-    //[SimpleJob(launchCount: 1, warmupCount: 1, targetCount: 1)]
+    [DisassemblyDiagnoser(printIL:true, recursiveDepth: 5)]
+    [SimpleJob(launchCount: 1, warmupCount: 1, targetCount: 1)]
     //[DryJob]
     public class BenchmarkMatrixMultiplication
     {
-        private const int MATRIX_SIZE = 1000;
+        private const int MATRIX_SIZE = 400;
         private readonly Random random = new Random(0);
         private readonly Matrix matrixA;
         private readonly Matrix matrixB;
         private readonly Matrix matrixRes;
 
-
-        private readonly Float.Matrix matrixFA;
-        private readonly Float.Matrix matrixFB;
-        private readonly Float.Matrix matrixFRes;
-
+        private readonly DenseMatrix mnMatrixA;
+        private readonly DenseMatrix mnMatrixB;
+        private readonly DenseMatrix mnMatrixRes;
 
         public BenchmarkMatrixMultiplication()
         {
             matrixA = Matrix.CreateRandomMatrix(random, MATRIX_SIZE);
             matrixB = Matrix.CreateRandomMatrix(random, MATRIX_SIZE);
             matrixRes = new Matrix(MATRIX_SIZE);
-
-            matrixFA = Float.Matrix.CreateRandomMatrix(random, MATRIX_SIZE);
-            matrixFB = Float.Matrix.CreateRandomMatrix(random, MATRIX_SIZE);
-            matrixFRes = new Float.Matrix(MATRIX_SIZE);
+            
+            mnMatrixA = (DenseMatrix)Matrix<double>.Build.Random(MATRIX_SIZE, MATRIX_SIZE);
+            mnMatrixB = (DenseMatrix)Matrix<double>.Build.Random(MATRIX_SIZE, MATRIX_SIZE);
+            mnMatrixRes = (DenseMatrix)Matrix<double>.Build.Dense(MATRIX_SIZE, MATRIX_SIZE);
         }
 
 
-        [Benchmark(Baseline = true)]
-        public void ProductWithTransposed() => matrixA.ProductWithTransposed(matrixB, matrixRes);
-        
+        [GlobalSetup(Target = nameof(MathNetNumericsManagedProduct))]
+        public void SetupManaged()
+        {
+            Control.UseSingleThread();
+            Control.UseManaged();
+        }
+
+        [GlobalSetup(Target = nameof(MathNetNumericsManagedReferenceProduct))]
+        public void SetupManagedReference()
+        {
+            Control.UseSingleThread();
+            Control.UseManagedReference();
+        }
+        [GlobalSetup(Target = nameof(MathNetNumericsMklProduct))]
+        public void SetupMkl()
+        {
+            Control.UseSingleThread();
+            Control.UseNativeMKL();
+        }
+
+        [GlobalSetup(Target = nameof(MathNetNumericsManagedProduct_MultiThreading))]
+        public void SetupManaged_MultiThreading()
+        {
+            Control.UseMultiThreading();
+            Control.UseManaged();
+        }
+
+        [GlobalSetup(Target = nameof(MathNetNumericsManagedReferenceProduct_MultiThreading))]
+        public void SetupManagedReference_MultiThreading()
+        {
+            Control.UseMultiThreading();
+            Control.UseManagedReference();
+        }
+        [GlobalSetup(Target = nameof(MathNetNumericsMklProduct_MultiThreading))]
+        public void SetupMkl_MultiThreading()
+        {
+            Control.UseMultiThreading();
+            Control.UseNativeMKL();
+        }
+
+        [Benchmark(Baseline = true), System.STAThread]
+        public void NaiveProduct() => matrixA.ProductWithTransposed(matrixB, matrixRes);
+
+        [Benchmark, System.STAThread]
+        public void MathNetNumericsManagedProduct() => mnMatrixA.TransposeAndMultiply(mnMatrixB, mnMatrixRes);
+        [Benchmark, System.STAThread]
+        public void MathNetNumericsManagedReferenceProduct() => mnMatrixA.TransposeAndMultiply(mnMatrixB, mnMatrixRes);
+        [Benchmark, System.STAThread]
+        public void MathNetNumericsMklProduct() => mnMatrixA.TransposeAndMultiply(mnMatrixB, mnMatrixRes);
+        [Benchmark, System.STAThread]
+        public void MathNetNumericsManagedProduct_MultiThreading() => mnMatrixA.TransposeAndMultiply(mnMatrixB, mnMatrixRes);
+        [Benchmark, System.STAThread]
+        public void MathNetNumericsManagedReferenceProduct_MultiThreading() => mnMatrixA.TransposeAndMultiply(mnMatrixB, mnMatrixRes);
+        [Benchmark, System.STAThread]
+        public void MathNetNumericsMklProduct_MultiThreading() => mnMatrixA.TransposeAndMultiply(mnMatrixB, mnMatrixRes);
         [Benchmark]
         public void ProductWithTransposed_Simd() => matrixA.ProductWithTransposed_Simd(matrixB, matrixRes);
-
+        [Benchmark]
+        public void MathNetNumerics_Simd() => mnMatrixA.TransposeAndMultiply(mnMatrixB, mnMatrixRes);
         [Benchmark]
         public void ProductWithTransposed_Span472() => matrixA.ProductWithTransposed_Span472(matrixB, matrixRes);
-
         [Benchmark]
         public void ProductWithTransposed_Unsafe() => matrixA.ProductWithTransposed_Unsafe(matrixB, matrixRes);
-
-        [Benchmark]
-        public void FProductWithTransposed() => matrixFA.ProductWithTransposed(matrixFB, matrixFRes);
-
-        //[Benchmark]
-        public void FProductWithTransposed_Simd() => matrixFA.ProductWithTransposed_Simd(matrixFB, matrixFRes);
-
-        //[Benchmark]
-        public void FProductWithTransposed_Span472() => matrixFA.ProductWithTransposed_Span472(matrixFB, matrixFRes);
-
-        //[Benchmark]
-        public void FProductWithTransposed_Unsafe() => matrixFA.ProductWithTransposed_Unsafe(matrixFB, matrixFRes);
-
-        //[Benchmark]
-        public void FProductWithTransposed_Matrix() => matrixFA.ProductWithTransposed_UseMatrix(matrixFB, matrixFRes);
     }
 }
